37bbdd08e859ab1c6b216ab8cd5ba511970164c4
RV <rv@TSL074s-MBP.tribalscale.lan>
2019-03-08 15:17:42 -0500

    Updates

---
+++ precache-manifest.0469d4aec3ae3a08000d23c8c69ec6d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b7c6435e8528afcd853",
+    "url": "/demo/static/css/main.e0805711.chunk.css"
+  },
+  {
+    "revision": "4b7c6435e8528afcd853",
+    "url": "/demo/static/js/main.acbbaf6f.chunk.js"
+  },
+  {
+    "revision": "b7e673b52c1f3a53650e",
+    "url": "/demo/static/js/runtime~main.4eca427f.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/demo/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2a9b88dea8670ad23df34b4aa7454624",
+    "url": "/demo/index.html"
+  }
+];
